8250e6b5fb7cdb954d3b2cdfc4624f3cdcd5f6a0
gabo34973180-cell <gabo34973180@gmail.com>
2026-05-20 16:54:34 -0600

    Produccion js

---
+++ javascript/modulos/dist/xyz.js
@@ -0,0 +1,2 @@
+(()=>{function n(o){return console.log("y:"+o),"fin y"}function l(o){return n("x"),console.log("x:"+o),"fin x"}function i(o){return"fin xx"}console.log(l("abc"));console.log(i("def"));console.log(n("ghi"));console.log((void 0)("jkl"));})();
+//# sourceMappingURL=xyz.js.map
